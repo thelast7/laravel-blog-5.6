8362f6647db522def9114e3a81d3dac720154195
thelast7 <takimanwar@gmail.com>
2018-05-28 11:32:44 +0700

    with ckeditor

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Unisharp\Ckeditor\ServiceProvider::class,
 
         /*
          * Application Service Providers...
